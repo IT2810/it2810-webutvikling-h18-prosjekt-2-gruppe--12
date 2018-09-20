5a558c17be075b71b703d204f92adf36ed76c6d0
ulvestad <simenu95@gmail.com>
2018-09-20 12:06:06 +0200

    responsive design using flex #22 and CSS styling #18 + bug fix #32

--- gruppe12-p2/src/App.js
+++ gruppe12-p2/src/App.js
@@ -6,92 +6,90 @@ import Tabs from "./components/tabs";
 import Tab from "./components/tab";
 import Selector from "./components/selector";
 import Header from "./components/header";
-import Footer from "./components/footer";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            musicSelected: "",
-            svgSelected: "",
-            textSelected: "",
-            activeTab: 0
-        };
-    }
-
-    handleCallbackFromSelector = selected => {
-        if (selected) {
-            let section = selected.split(" ")[0];
-            let category = selected.split(" ")[1];
-            if (section === "Music") {
-                this.setState({
-                    musicSelected: category
-                });
-            } else if (section === "SVG") {
-                this.setState({
-                    svgSelected: category
-                });
-            } else {
-                this.setState({
-                    textSelected: category
-                });
-            }
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      musicSelected: "",
+      svgSelected: "",
+      textSelected: "",
+      activeTab: 0
     };
+  }
 
-    handleCallbackFromTabs = activeTab => {
+  handleCallbackFromSelector = selected => {
+    if (selected) {
+      let section = selected.split(" ")[0];
+      let category = selected.split(" ")[1];
+      if (section === "Music") {
         this.setState({
-            activeTab: activeTab
+          musicSelected: category
         });
-    };
-
-    render() {
-        return (
-            <div className="App">
-                <Header/>
-                <Tabs
-                    getActiveTab={activeTab => this.handleCallbackFromTabs(activeTab)}
-                >
-                    <Tab
-                        label="Tab 1"
-                        tabIndex={1}
-                        musicSelected={this.state.musicSelected}
-                        svgSelected={this.state.svgSelected}
-                        textSelected={this.state.textSelected}
-                        activeTab={this.state.activeTab}
-                    />
-                    <Tab
-                        label="Tab 2"
-                        tabIndex={2}
-                        musicSelected={this.state.musicSelected}
-                        svgSelected={this.state.svgSelected}
-                        textSelected={this.state.textSelected}
-                        activeTab={this.state.activeTab}
-                    />
-                    <Tab
-                        label="Tab 3"
-                        tabIndex={3}
-                        musicSelected={this.state.musicSelected}
-                        svgSelected={this.state.svgSelected}
-                        textSelected={this.state.textSelected}
-                        activeTab={this.state.activeTab}
-                    />
-                    <Tab
-                        label="Tab 4"
-                        tabIndex={4}
-                        musicSelected={this.state.musicSelected}
-                        svgSelected={this.state.svgSelected}
-                        textSelected={this.state.textSelected}
-                        activeTab={this.state.activeTab}
-                    />
-                </Tabs>
-                <Selector
-                    getSelected={selected => this.handleCallbackFromSelector(selected)}
-                />
-                <Footer/>
-            </div>
-        );
+      } else if (section === "SVG") {
+        this.setState({
+          svgSelected: category
+        });
+      } else {
+        this.setState({
+          textSelected: category
+        });
+      }
     }
+  };
+
+  handleCallbackFromTabs = activeTab => {
+    this.setState({
+      activeTab: activeTab
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Tabs
+          getActiveTab={activeTab => this.handleCallbackFromTabs(activeTab)}
+        >
+          <Tab
+            label="Tab 1"
+            tabIndex={1}
+            musicSelected={this.state.musicSelected}
+            svgSelected={this.state.svgSelected}
+            textSelected={this.state.textSelected}
+            activeTab={this.state.activeTab}
+          />
+          <Tab
+            label="Tab 2"
+            tabIndex={2}
+            musicSelected={this.state.musicSelected}
+            svgSelected={this.state.svgSelected}
+            textSelected={this.state.textSelected}
+            activeTab={this.state.activeTab}
+          />
+          <Tab
+            label="Tab 3"
+            tabIndex={3}
+            musicSelected={this.state.musicSelected}
+            svgSelected={this.state.svgSelected}
+            textSelected={this.state.textSelected}
+            activeTab={this.state.activeTab}
+          />
+          <Tab
+            label="Tab 4"
+            tabIndex={4}
+            musicSelected={this.state.musicSelected}
+            svgSelected={this.state.svgSelected}
+            textSelected={this.state.textSelected}
+            activeTab={this.state.activeTab}
+          />
+        </Tabs>
+        <Selector
+          getSelected={selected => this.handleCallbackFromSelector(selected)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
